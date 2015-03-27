40a9704d24044f8162f7e866141ee35544d653e1
Steven Shyun <stevenshuhyow@yahoo.com>
2015-03-27 10:45:25 -0700

    Finished LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.tail === null){
+      // if LL is empty...do this
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+      //otherwise do this
+    }
   };
 
   list.removeHead = function(){
+    var removed = list.head.value
+    list.head = list.head.next;
+    return removed
   };
 
   list.contains = function(target){
+    var currentnode = list.head
+
+
+    while(currentnode.next !== null){
+      if(target === currentnode.value){
+        return true
+      }
+      currentnode = currentnode.next
+    }
+    if(list.tail.value === target){
+      return true
+    }
+    return false
   };
 
   return list;
